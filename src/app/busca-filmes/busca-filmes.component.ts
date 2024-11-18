97e9e43dc539bed9d834dd18caacab97446d49a9
Dayane <dayane.lima@escolar.ifrn.edu.br>
2024-11-18 14:22:43 -0300

    botões

--- src/app/busca-filmes/busca-filmes.component.ts
+++ src/app/busca-filmes/busca-filmes.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, 
+        FormGroup, 
+        Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-busca-filmes',
@@ -6,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrl: './busca-filmes.component.css'
 })
 export class BuscaFilmesComponent {
+
+  formBusca: FormGroup 
+  constructor(private fb: FormBuilder){
+    this.formBusca = this.fb.group({
+      titulo: ['', [Validators.required,
+                    Validators.minLength(2)]] /*Digitar pelo menos 1 letra*/
+    })
+  }
+
+  buscar(){
+    const titulo = this.formBusca.value.titulo /*retorna um objeto json*/
+
+    console.log(titulo)
+  }
 
 }
